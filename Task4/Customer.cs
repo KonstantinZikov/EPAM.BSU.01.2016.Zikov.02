79f08861ad86a5ab2401254fd4ccc9030e726451
Konstantin Zikov <Konstantin.Zikov.S@gmail.com>
2016-03-14 00:05:49 +0300

    Task4 and Task4Tests added. Also there are some little changes for Task2 and Task3.

---
+++ Task4/Customer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string ContactPhone { get; set; }
+        public decimal Revenue { get; set; }
+
+
+        public override string ToString()
+        {
+            return ToString();
+        }
+
+        public string ToString(IFormatProvider provider = null)
+        {
+            return "Customer record: " +
+                Name + ", " + ContactPhone + ", " + Revenue.ToString(provider) + ".";
+        }
+
+        public string ToString(string format, IFormatProvider provider = null)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < format.Length;i++)
+            {
+                if (format[i] == '$' && i < format.Length - 1)
+                {
+                    i++;
+                    switch(format[i])
+                    {
+                        case 'N':
+                            sb.Append(Name);
+                            break;
+                        case 'P':
+                            sb.Append(ContactPhone);
+                            break;
+                        case 'R':
+                            sb.Append(Revenue.ToString(provider));
+                            break;
+                        case '$':
+                            sb.Append('$');
+                            break;
+                        default:
+                            sb.Append('$');
+                            sb.Append(format[i]);
+                            break;
+                    }
+                }
+                else
+                {
+                    sb.Append(format[i]);
+                }
+            }
+            return sb.ToString();        
+        }
+    }
+}
---
+++ Task4Tests/CustomerTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task4;
+using System.Globalization;
+namespace Task4Tests
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void ToString_FormatWithoutProvider_FormattedView()
+        {
+            //arrange
+            var customer = new Customer()
+            {
+                Name = "Konstantin Zikov",
+                ContactPhone = "+375 (29) 9999999",
+                Revenue = 1000.000m
+            };
+            string expected = "Record: Konstantin Zikov, +375 (29) 9999999, 1000.000.";
+            //act
+            string actual = customer.ToString("Record: $N, $P, $R.");
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ToString_FormatWithGermanProvider_GermanFormattedRevenueView()
+        {
+            //arrange
+            var customer = new Customer()
+            {
+                Name = "Konstantin Zikov",
+                ContactPhone = "+375 (29) 9999999",
+                Revenue = 1000.000m
+            };
+            string expected = "Revenue: 1000,000.";
+            var ci = new CultureInfo("de-DE");
+            //act
+            string actual = customer.ToString("Revenue: $R.", ci);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ToString_FormatWithDoubleDollarSymbol_OneDollarSymbolInView()
+        {
+            //arrange
+            var customer = new Customer()
+            {
+                Name = "Konstantin Zikov",
+                ContactPhone = "+375 (29) 9999999",
+                Revenue = 1000.000m
+            };
+            string expected = "$";
+            //act
+            string actual = customer.ToString("$$");
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ToString_FormatWithUnsupportedSymbolAfterDollar_DollarAndSymbolInView()
+        {
+            //arrange
+            var customer = new Customer()
+            {
+                Name = "Konstantin Zikov",
+                ContactPhone = "+375 (29) 9999999",
+                Revenue = 1000.000m
+            };
+            string expected = "$Y";
+            //act
+            string actual = customer.ToString("$Y");
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ToString_WithGermanProvider_GermanFormattedRevenueView()
+        {
+            //arrange
+            var customer = new Customer()
+            {
+                Name = "Konstantin Zikov",
+                ContactPhone = "+375 (29) 9999999",
+                Revenue = 1000.000m
+            };
+            string expected = "Customer record: Konstantin Zikov, +375 (29) 9999999, 1000,000";
+            var ci = new CultureInfo("de-DE");
+
+            //act
+            string actual = customer.ToString(ci);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ToString_OverridedMethod_FormattedByDefaultProvider()
+        {
+            //arrange
+            object customer = new Customer()
+            {
+                Name = "Konstantin Zikov",
+                ContactPhone = "+375 (29) 9999999",
+                Revenue = 1000.000m
+            };
+            string expected = "Customer record: Konstantin Zikov, +375 (29) 9999999, 1000.000.";
+
+            //act
+            string actual = customer.ToString();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
