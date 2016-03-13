e5b2d47d645267da1db4552703e5447894559353
Konstantin Zikov <Konstantin.Zikov.S@gmail.com>
2016-03-13 19:22:39 +0300

    Task2 and Task2Tests added

---
+++ Task2/GcdFunctions.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Task2
+{
+    public static class GcdFunctions
+    {
+        #region Euclidean algorithm
+        /// <summary>
+        /// Returns the greatest common divisor of two numbers. GCD is calculated with Euclidean algorithm.
+        /// Both of numbers must be not negative.
+        /// Also returns the calculating time as an out parameter.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when firstNumber or secondNumber is less then or equal to 0;</exception>
+        /// <param name="time">
+        /// After the execution of the method this argument will contain the time spent on calculation.</param>
+        /// <param name="firstNumber">The first number for GCD algorythm.</param>
+        /// <param name="secondNumber">The second number for GCD algorythm.</param>
+        /// <returns>The GCD of two specified numbers</returns>
+        public static int Gcd(out double time, int firstNumber, int secondNumber)
+        {
+            if (firstNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("firstNumber must be greater then 0.");
+            }
+            if (secondNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("secondNumber must be greater then 0.");
+            }
+            timer.Restart();
+            var result = firstNumber = PureGcd(firstNumber, secondNumber);
+            timer.Stop();
+            time = timer.Elapsed.TotalMilliseconds;
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the greatest common divisor of three numbers. GCD is calculated with Euclidean algorithm.
+        /// All numbers must be not negative.
+        /// Also returns the calculating time as an out parameter.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when one of numbers is less then or equal to 0;</exception>
+        /// <param name="time">
+        /// After the execution of the method this argument will contain the time spent on calculation.</param>
+        /// <param name="firstNumber">The first number for GCD algorythm.</param>
+        /// <param name="secondNumber">The second number for GCD algorythm.</param>
+        /// <param name="thirdNumber">The third number for GCD algorythm.</param>
+        /// <returns>The GCD of three specified numbers</returns>
+        public static int Gcd(out double time, int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("firstNumber must be greater then 0.");
+            }
+            if (secondNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("secondNumber must be greater then 0.");
+            }
+            if (thirdNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("thirdNumber must be greater then 0.");
+            }
+            timer.Restart();
+            var result = PureGcd(PureGcd(firstNumber, secondNumber), thirdNumber);
+            timer.Stop();
+            time = timer.ElapsedMilliseconds;
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the greatest common divisor of the set of numbers. GCD is calculated with Euclidean algorithm.
+        /// All numbers must be not negative.
+        /// Also returns the calculating time as an out parameter.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when one of numbers is less then or equal to 0;
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when the numbers array is null;
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the count of numbers is less then 2.
+        /// </exception>
+        /// <param name="time">
+        /// After the execution of the method this argument will contain the time spent on calculation.</param>
+        /// <param name="numbers">The specified numbers for GCD algorythm.</param>
+        /// <returns>The GCD of couple of numbers</returns>
+        public static int Gcd(out double time, params int[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException("numbers is null.");
+            }
+            if (numbers.Length < 2)
+            {
+                throw new ArgumentException("The GCD requires at least two numbers.");
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    throw new ArgumentOutOfRangeException($"The number at index {i} must be greater then 0");
+                }
+            }
+            timer.Restart();
+            int lastGcd = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                lastGcd = PureGcd(lastGcd, numbers[i]);
+            }
+            timer.Stop();
+            time = timer.Elapsed.TotalMilliseconds;
+            return lastGcd;
+        }
+        #endregion
+
+        #region Binary Euclidean algorithm
+        /// <summary>
+        /// Returns the greatest common divisor of two numbers. GCD is calculated with binary Euclidean algorithm.
+        /// Both of numbers must be not negative.
+        /// Also returns the calculating time as an out parameter.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when firstNumber or secondNumber is less then 0;</exception>
+        /// <param name="time">
+        /// After the execution of the method this argument will contain the time spent on calculation.</param>
+        /// <param name="firstNumber">The first number for GCD algorythm.</param>
+        /// <param name="secondNumber">The second number for GCD algorythm.</param>
+        /// <returns>The GCD of two specified numbers</returns>
+        public static int BinaryGcd(out double time, int firstNumber, int secondNumber)
+        {
+            if (firstNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("firstNumber must be greater then -1.");
+            }
+            if (secondNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("secondNumber must be greater then -1.");
+            }
+            timer.Restart();
+            var result = firstNumber = PureBinaryGcd(firstNumber, secondNumber);
+            timer.Stop();
+            time = timer.Elapsed.TotalMilliseconds;
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the greatest common divisor of three numbers. GCD is calculated with binary Euclidean algorithm.
+        /// All numbers must be not negative.
+        /// Also returns the calculating time as an out parameter.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when one of numbers is less then 0;</exception>
+        /// <param name="time">
+        /// After the execution of the method this argument will contain the time spent on calculation.</param>
+        /// <param name="firstNumber">The first number for GCD algorythm.</param>
+        /// <param name="secondNumber">The second number for GCD algorythm.</param>
+        /// <param name="thirdNumber">The third number for GCD algorythm.</param>
+        /// <returns>The GCD of three specified numbers</returns>
+        public static int BinaryGcd(out double time, int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("firstNumber must be greater then -1.");
+            }
+            if (secondNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("secondNumber must be greater then -1.");
+            }
+            if (thirdNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("thirdNumber must be greater then -1.");
+            }
+            timer.Restart();
+            var result = PureBinaryGcd(PureBinaryGcd(firstNumber, secondNumber), thirdNumber);
+            timer.Stop();
+            time = timer.Elapsed.TotalMilliseconds;
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the greatest common divisor of the set of numbers. GCD is calculated with binary Euclidean algorithm.
+        /// All numbers must be not negative.
+        /// Also returns the calculating time as an out parameter.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when one of numbers is less then 0;
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when the numbers array is null;
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the count of numbers is less then 2.
+        /// </exception>
+        /// <param name="time">
+        /// After the execution of the method this argument will contain the time spent on calculation.</param>
+        /// <param name="numbers">The specified numbers for GCD algorythm.</param>
+        /// <returns>The GCD of couple of numbers</returns>
+        public static int BinaryGcd(out double time, params int[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException("numbers is null.");
+            }
+            if (numbers.Length < 2)
+            {
+                throw new ArgumentException("The GCD requires at least two numbers.");
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    throw new ArgumentOutOfRangeException($"The number at index {i} must be greater then -1");
+                }
+            }
+            timer.Restart();
+            int lastGcd = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                lastGcd = PureBinaryGcd(lastGcd, numbers[i]);
+            }
+            timer.Stop();
+            time = timer.Elapsed.TotalMilliseconds;
+            return lastGcd;
+        }
+        #endregion
+
+        #region Private parts
+        // The pure gcd algorythm without validation and timer. 
+        private static int PureGcd(int firstNumber, int secondNumber)
+        {
+            while (secondNumber != 0)
+            {
+                secondNumber = firstNumber % (firstNumber = secondNumber);
+            }
+            return firstNumber;
+        }
+
+        // The pure binary gcd algorythm without validation and timer.
+        private static int PureBinaryGcd(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == secondNumber || secondNumber == 0)
+            {
+                return firstNumber;
+            }
+            if (firstNumber == 0)
+            {
+                return secondNumber;
+            }
+
+            if ((firstNumber & 1) != 1)// Parity check.
+            {
+                return (secondNumber & 1) != 1 ?
+                    2 * PureBinaryGcd(firstNumber / 2, secondNumber / 2) :
+                    PureBinaryGcd(firstNumber / 2, secondNumber);
+            }
+            if ((secondNumber & 1) != 1)
+            {
+                return PureBinaryGcd(firstNumber, secondNumber / 2);
+            }
+            else
+            {
+                return (firstNumber > secondNumber) ?
+                    PureBinaryGcd((firstNumber - secondNumber) / 2, secondNumber) :
+                    PureBinaryGcd((secondNumber - firstNumber) / 2, firstNumber);
+            }
+        }
+
+        private static Stopwatch timer = new Stopwatch();
+        #endregion
+    }
+}
---
+++ Task2Tests/GcdFunctionsTests.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Task2.GcdFunctions;
+
+namespace Task2Tests
+{ 
+    [TestClass]
+    public class GcdFunctionsTests
+    {
+
+        #region Gcd tests
+        [TestMethod]
+        public void Gcd_Put42and18_Expected6()
+        {
+            //arrange
+            int a = 42;
+            int b = 18;
+            double time;
+
+            int expected = 6;
+
+            //act
+            int actual = Gcd(out time, a, b);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Gcd_Put42andMinus3_ArgumentOutOfRangeExceptionExpected()
+        {
+            //arrange
+            int a = 42;
+            int b = -3;
+            double time;
+
+            //act
+            int actual = Gcd(out time, a, b);
+
+            //assert are handled by exception
+        }
+
+        [TestMethod]
+        public void Gcd_Put42and18and21_Expected3()
+        {
+            //arrange
+            int a = 42;
+            int b = 18;
+            int c = 21;
+            double time;
+
+            int expected = 3;
+
+            //act
+            int actual = Gcd(out time, a, b, c);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Gcd_PutMinus15and18and21_ArgumentOutOfRangeExceptionExpected()
+        {
+            //arrange
+            int a = -15;
+            int b = 18;
+            int c = 21;
+            double time;
+
+            //act
+            int actual = Gcd(out time, a, b, c);
+
+            //assert are handled by exception
+        }
+
+        [TestMethod]
+        public void Gcd_Put42and18and21and5_Expected1()
+        {
+            //arrange
+            int a = 42;
+            int b = 18;
+            int c = 21;
+            int d = 5;
+            double time;
+
+            int expected = 1;
+
+            //act
+            int actual = Gcd(out time, a, b, c, d);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Gcd_PutNullArray_ArgumentNullExceptionExpected()
+        {
+            //arrange
+            int[] array = null;
+            double time;
+
+            //act
+            int actual = Gcd(out time, array);
+
+            //assert is handled by exception
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Gcd_PutArrayWithNegativeElement_ArgumentOutOfRangeExceptionExpected()
+        {
+            //arrange
+            int[] array = new[] {17,5,-7,23};
+            double time;
+
+            //act
+            int actual = Gcd(out time, array);
+
+            //assert is handled by exception
+        }
+
+        #endregion
+
+        #region BinaryGcd tests
+        [TestMethod]
+        public void BinaryGcd_Put74and8_Expected2()
+        {
+            //arrange
+            int a = 74;
+            int b = 8;
+            double time;
+
+            int expected = 2;
+
+            //act
+            int actual = BinaryGcd(out time, a, b);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BinaryGcd_PutMinus78and15_ArgumentOutOfRangeExceptionExpected()
+        {
+            //arrange
+            int a = -78;
+            int b = 15;
+            double time;
+
+            //act
+            int actual = BinaryGcd(out time, a, b);
+
+            //assert are handled by exception
+        }
+
+        [TestMethod]
+        public void BinaryGcd_Put42and18and21_Expected3()
+        {
+            //arrange
+            int a = 100;
+            int b = 10;
+            int c = 45;
+            double time;
+
+            int expected = 5;
+
+            //act
+            int actual = BinaryGcd(out time, a, b, c);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BinaryGcd_Put0and18and21_Expected6()
+        {
+            //arrange
+            int a = 0;
+            int b = 18;
+            int c = 24;
+            double time;
+
+            int expected = 6;
+
+            //act
+            int actual = BinaryGcd(out time, a, b, c);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BinaryGcd_Put42and18and21and5_Expected1()
+        {
+            //arrange
+            int a = 42;
+            int b = 18;
+            int c = 21;
+            int d = 5;
+            double time;
+
+            int expected = 1;
+
+            //act
+            int actual = BinaryGcd(out time, a, b, c, d);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BinaryGcd_PutNullArray_ArgumentNullExceptionExpected()
+        {
+            //arrange
+            int[] array = null;
+            double time;
+
+            //act
+            int actual = BinaryGcd(out time, array);
+
+            //assert is handled by exception
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BinaryGcd_PutArrayWithElementEqualToMinus1_ArgumentOutOfRangeExceptionExpected()
+        {
+            //arrange
+            int[] array = new[] { 17, 5, -1, 23 };
+            double time;
+
+            //act
+            int actual = BinaryGcd(out time, array);
+
+            //assert is handled by exception
+        }
+        #endregion
+    }
+}
